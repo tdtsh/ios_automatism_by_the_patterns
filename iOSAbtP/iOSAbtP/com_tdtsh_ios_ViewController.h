92b433905fddcb3d7e1fc5a0fb11483383194ca8
tdtsh <systemsfailing@gmail.com>
2012-08-30 13:55:53 +0900

    created

---
+++ iOSAbtP/iOSAbtP/com_tdtsh_ios_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  com_tdtsh_ios_ViewController.h
+//  iOSAbtP
+//
+//  Created by 花崎 忠利 on 12/08/30.
+//  Copyright (c) 2012年 tadatoshi_hanazaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_tdtsh_ios_ViewController : UIViewController
+
+@end
---
+++ iOSAbtP/iOSAbtP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSAbtP
+//
+//  Created by 花崎 忠利 on 12/08/30.
+//  Copyright (c) 2012年 tadatoshi_hanazaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "com_tdtsh_ios_AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([com_tdtsh_ios_AppDelegate class]));
+    }
+}
